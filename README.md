046763730eb65704f88d27847f58b0c9d9189b90
Vidar Eldøy <vidar@eldoy.com>
2023-01-12 11:10:28 +0100

    Initial version

--- README.md
+++ README.md
@@ -2,4 +2,45 @@
 
 SVG spinners and loaders.
 
+### Install
+```
+npm i spinn
+```
+
+### Usage
+
+Require lib:
+```js
+const spinn = require('spinn')
+```
+
+Include in web page:
+```
+<script>${spinn}</spinn>
+```
+
+Use on page:
+```js
+// Create spinner, showing default options
+var spinner = spinn({
+  // Where to insert the spinner
+  el: 'spinn',
+  // The color of the spinner
+  color: 'tomato'
+})
+
+// Start spinner
+spinner.start()
+
+// Stop spinner
+spinner.stop()
+
+// Short cut, using default options
+var spinner = spinn().start()
+
+// Stop after 5 seconds
+setTimeout(spinner.stop, 5000)
+```
+
+
 MIT Licensed. Enjoy!
---
+++ test/server.js
@@ -0,0 +1,33 @@
+const http = require('http')
+const spinn = require('../index.js')
+
+function page() {
+  return /* HTML */ `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Spinn</title>
+      </head>
+      <body>
+        <spinn></spinn>
+        <script>
+          ${spinn}
+          var spinner = spinn().start()
+          setTimeout(spinner.stop, 5000)
+        </script>
+      </body>
+    </html>
+  `
+}
+
+const server = http.createServer(function (req, res) {
+  res.setHeader('content-type', 'text/html')
+  res.end(page())
+})
+
+server.listen(63672)
+const port = server.address().port
+console.log(`http://localhost:${port}`)
